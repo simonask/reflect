3010265f422f436c2180392e68821ab405c5cac0
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-05-13 10:09:52 +0200

    A simple Maybe<T> class for non-heap-allocated optional return values.

---
+++ maybe.hpp
@@ -0,0 +1,158 @@
+#pragma once
+#ifndef MAYBE_HPP_8R2MUT0P
+#define MAYBE_HPP_8R2MUT0P
+
+#include "basic.hpp"
+#include <type_traits>
+#include <new>
+
+template <typename T>
+class Maybe {
+public:
+	static const bool IsMoveConstructible = std::is_move_constructible<T>::value;
+	static const bool IsMoveAssignable = std::is_move_assignable<T>::value;
+	static const bool IsCopyConstructible = std::is_copy_constructible<T>::value;
+	static const bool IsCopyAssignable = std::is_copy_assignable<T>::value;
+	
+	Maybe() : ptr_(nullptr) {}
+	Maybe(const Maybe<T>& other);
+	Maybe(Maybe<T>&& other);
+	Maybe(const T& other);
+	Maybe(T&& other);
+	~Maybe() { clear(); }
+	Maybe<T>& operator=(const Maybe<T>& other);
+	Maybe<T>& operator=(Maybe<T>&& other);
+	Maybe<T>& operator=(const T& other);
+	Maybe<T>& operator=(T&& other);
+	
+	void clear();
+	const T* get() const { return ptr_; }
+	T* get() { return ptr_; }
+	T* operator->() { return get(); }
+	const T* operator->() const { return get(); }
+	T& operator*() { return *get(); }
+	const T& operator*() const { return *get(); }
+	
+	explicit operator bool() const { return get() != nullptr; }
+private:
+	T* ptr_;
+	struct Placeholder {
+		byte _[sizeof(T)];
+	};
+	Placeholder memory_;
+	
+	const T* memory() const { return reinterpret_cast<const T*>(&memory_); }
+	T* memory() { return reinterpret_cast<T*>(&memory_); }
+	
+	template <typename U = T>
+	typename std::enable_if<IsCopyConstructibleNonRef<U>::Value && IsCopyAssignableNonRef<U>::Value>::type
+	assign(const T& by_copy) {
+		if (ptr_ != nullptr) {
+			*memory() = by_copy;
+		} else {
+			ptr_ = ::new(memory()) T(by_copy);
+		}
+	}
+	
+	template <typename U = T>
+	typename std::enable_if<IsCopyConstructibleNonRef<U>::Value && !IsCopyAssignableNonRef<U>::Value>::type
+	assign(const T& by_copy) {
+		if (ptr_ != nullptr) {
+			clear();
+		}
+		ptr_ = ::new(memory()) T(by_copy);
+	}
+	
+	template <typename U = T>
+	typename std::enable_if<!IsCopyConstructibleNonRef<U>::Value && IsCopyAssignableNonRef<U>::Value>::type
+	assign(const T& by_copy) {
+		if (ptr_ == nullptr) {
+			ptr_ = ::new(memory()) T;
+		}
+		*memory() = by_copy;
+	}
+	
+	template <typename U = T>
+	typename std::enable_if<IsMoveConstructibleNonRef<U>::Value && IsMoveAssignableNonRef<U>::Value>::type
+	assign(T&& by_move) {
+		if (ptr_ != nullptr) {
+			*memory() = std::move(by_move);
+		} else {
+			ptr_ = ::new(memory()) T(std::move(by_move));
+		}
+	}
+	
+	template <typename U = T>
+	typename std::enable_if<IsMoveConstructibleNonRef<U>::Value && !IsMoveAssignableNonRef<U>::Value>::type
+	assign(T&& by_move) {
+		if (ptr_ != nullptr) {
+			clear();
+		}
+		ptr_ = ::new(memory()) T(std::move(by_move));
+	}
+	
+	template <typename U = T>
+	typename std::enable_if<!IsMoveConstructibleNonRef<U>::Value && IsMoveAssignableNonRef<U>::Value>::type
+	assign(T&& by_move) {
+		if (ptr_ == nullptr) {
+			ptr_ = ::new(memory()) T;
+		}
+		*memory() = std::move(by_move);
+	}
+};
+
+template <typename T>
+Maybe<T>::Maybe(const Maybe<T>& other) : ptr_(nullptr) {
+	if (other) assign(*other);
+}
+
+template <typename T>
+Maybe<T>::Maybe(Maybe<T>&& other) : ptr_(nullptr) {
+	if (other) assign(std::move(*other));
+}
+
+template <typename T>
+Maybe<T>::Maybe(const T& other) : ptr_(nullptr) {
+	assign(other);
+}
+
+template <typename T>
+Maybe<T>::Maybe(T&& other) : ptr_(nullptr) {
+	assign(std::move(other));
+}
+
+template <typename T>
+Maybe<T>& Maybe<T>::operator=(const Maybe<T>& other) {
+	if (other) assign(*other);
+	else clear();
+	return *this;
+}
+
+template <typename T>
+Maybe<T>& Maybe<T>::operator=(Maybe<T>&& other) {
+	if (*other) assign(std::move(other));
+	else clear();
+	return *this;
+}
+
+template <typename T>
+Maybe<T>& Maybe<T>::operator=(const T& other) {
+	assign(other);
+	return *this;
+}
+
+template <typename T>
+Maybe<T>& Maybe<T>::operator=(T&& other) {
+	assign(std::move(other));
+	return *this;
+}
+
+template <typename T>
+void Maybe<T>::clear() {
+	if (ptr_ != nullptr) {
+		memory()->~T();
+		ptr_ = nullptr;
+	}
+}
+
+#endif /* end of include guard: MAYBE_HPP_8R2MUT0P */
---
+++ test/maybe_test.cpp
@@ -0,0 +1,237 @@
+#include "maybe.hpp"
+
+struct TrivialType {
+	TrivialType(int n = 0) : n(n) {}
+	TrivialType(const TrivialType& other) = default;
+	TrivialType(TrivialType&& other) = default;
+	TrivialType& operator=(const TrivialType&) = default;
+	TrivialType& operator=(TrivialType&&) = default;
+	int n;
+};
+
+struct OnlyCopyConstructible {
+	OnlyCopyConstructible(int n = 0) : n(n) {}
+	OnlyCopyConstructible(const OnlyCopyConstructible& other) = default;
+	OnlyCopyConstructible(OnlyCopyConstructible&&) = delete;
+	OnlyCopyConstructible& operator=(const OnlyCopyConstructible&) = delete;
+	OnlyCopyConstructible& operator=(OnlyCopyConstructible&&) = delete;
+	int n;
+};
+
+struct OnlyMoveConstructible {
+	OnlyMoveConstructible(int n = 0) : n(n) {}
+	//OnlyMoveConstructible(const OnlyMoveConstructible&) = delete;
+	OnlyMoveConstructible(OnlyMoveConstructible&& other) { n = other.n; }
+	OnlyMoveConstructible& operator=(const OnlyMoveConstructible&) = delete;
+	OnlyMoveConstructible& operator=(OnlyMoveConstructible&&) = delete;
+	int n;
+};
+
+struct OnlyCopyAssignable {
+	OnlyCopyAssignable(int n = 0) : n(n) {}
+	OnlyCopyAssignable(const OnlyCopyAssignable& other) = delete;
+	OnlyCopyAssignable(OnlyCopyAssignable&&) = delete;
+	OnlyCopyAssignable& operator=(const OnlyCopyAssignable&) = default;
+	OnlyCopyAssignable& operator=(OnlyCopyAssignable&&) = delete;
+	int n;
+};
+
+struct OnlyMoveAssignable {
+	OnlyMoveAssignable(int n = 0) : n(n) {}
+	OnlyMoveAssignable(const OnlyMoveAssignable& other) = delete;
+	OnlyMoveAssignable(OnlyMoveAssignable&&) = delete;
+	OnlyMoveAssignable& operator=(const OnlyMoveAssignable&) = delete;
+	OnlyMoveAssignable& operator=(OnlyMoveAssignable&&) = default;
+	int n;
+};
+
+struct CopyConstructibleAndAssignable {
+	CopyConstructibleAndAssignable(int n = 0) : n(n) {}
+	CopyConstructibleAndAssignable(const CopyConstructibleAndAssignable& other) = default;
+	CopyConstructibleAndAssignable(CopyConstructibleAndAssignable&&) = delete;
+	CopyConstructibleAndAssignable& operator=(const CopyConstructibleAndAssignable& other) = default;
+	CopyConstructibleAndAssignable& operator=(CopyConstructibleAndAssignable&&) = delete;
+	int n;
+};
+
+struct MoveConstructibleAndAssignable {
+	MoveConstructibleAndAssignable(int n = 0) : n(n) {}
+	MoveConstructibleAndAssignable(const MoveConstructibleAndAssignable& other) = delete;
+	MoveConstructibleAndAssignable(MoveConstructibleAndAssignable&&) = default;
+	MoveConstructibleAndAssignable& operator=(const MoveConstructibleAndAssignable& other) = delete;
+	MoveConstructibleAndAssignable& operator=(MoveConstructibleAndAssignable&&) = default;
+	int n;
+};
+
+struct CopyConstructibleAndMoveAssignable {
+	CopyConstructibleAndMoveAssignable(int n = 0) : n(n) {}
+	CopyConstructibleAndMoveAssignable(const CopyConstructibleAndMoveAssignable& other) = default;
+	CopyConstructibleAndMoveAssignable(CopyConstructibleAndMoveAssignable&&) = delete;
+	CopyConstructibleAndMoveAssignable& operator=(const CopyConstructibleAndMoveAssignable& other) = delete;
+	CopyConstructibleAndMoveAssignable& operator=(CopyConstructibleAndMoveAssignable&&) = default;
+	int n;
+};
+
+struct MoveConstructibleAndCopyAssignable {
+	MoveConstructibleAndCopyAssignable(int n = 0) : n(n) {}
+	MoveConstructibleAndCopyAssignable(const MoveConstructibleAndCopyAssignable& other) = delete;
+	MoveConstructibleAndCopyAssignable(MoveConstructibleAndCopyAssignable&&) = default;
+	MoveConstructibleAndCopyAssignable& operator=(const MoveConstructibleAndCopyAssignable& other) = default;
+	MoveConstructibleAndCopyAssignable& operator=(MoveConstructibleAndCopyAssignable&&) = delete;
+	int n;
+};
+
+struct CopyAndMoveConstructibleCopyAssignable {
+	CopyAndMoveConstructibleCopyAssignable(int n = 0) : n(n) {}
+	CopyAndMoveConstructibleCopyAssignable(const CopyAndMoveConstructibleCopyAssignable& other) = default;
+	CopyAndMoveConstructibleCopyAssignable(CopyAndMoveConstructibleCopyAssignable&&) = default;
+	CopyAndMoveConstructibleCopyAssignable& operator=(const CopyAndMoveConstructibleCopyAssignable& other) = default;
+	CopyAndMoveConstructibleCopyAssignable& operator=(CopyAndMoveConstructibleCopyAssignable&&) = delete;
+	int n;
+};
+
+struct CopyAndMoveConstructibleMoveAssignable {
+	CopyAndMoveConstructibleMoveAssignable(int n = 0) : n(n) {}
+	CopyAndMoveConstructibleMoveAssignable(const CopyAndMoveConstructibleMoveAssignable& other) = default;
+	CopyAndMoveConstructibleMoveAssignable(CopyAndMoveConstructibleMoveAssignable&&) = default;
+	CopyAndMoveConstructibleMoveAssignable& operator=(const CopyAndMoveConstructibleMoveAssignable& other) = delete;
+	CopyAndMoveConstructibleMoveAssignable& operator=(CopyAndMoveConstructibleMoveAssignable&&) = default;
+	int n;
+};
+
+struct CopyConstructibleCopyAndMoveAssignable {
+	CopyConstructibleCopyAndMoveAssignable(int n = 0) : n(n) {}
+	CopyConstructibleCopyAndMoveAssignable(const CopyConstructibleCopyAndMoveAssignable& other) = default;
+	CopyConstructibleCopyAndMoveAssignable(CopyConstructibleCopyAndMoveAssignable&&) = delete;
+	CopyConstructibleCopyAndMoveAssignable& operator=(const CopyConstructibleCopyAndMoveAssignable& other) = default;
+	CopyConstructibleCopyAndMoveAssignable& operator=(CopyConstructibleCopyAndMoveAssignable&&) = default;
+	int n;
+};
+
+struct MoveConstructibleCopyAndMoveAssignable {
+	MoveConstructibleCopyAndMoveAssignable(int n = 0) : n(n) {}
+	MoveConstructibleCopyAndMoveAssignable(const MoveConstructibleCopyAndMoveAssignable& other) = delete;
+	MoveConstructibleCopyAndMoveAssignable(MoveConstructibleCopyAndMoveAssignable&&) = default;
+	MoveConstructibleCopyAndMoveAssignable& operator=(const MoveConstructibleCopyAndMoveAssignable& other) = default;
+	MoveConstructibleCopyAndMoveAssignable& operator=(MoveConstructibleCopyAndMoveAssignable&&) = default;
+	int n;
+};
+
+struct NoDefaultConstructor {
+	explicit NoDefaultConstructor(int n) : n(n) {}
+	int n;
+};
+
+
+template <typename T>
+void run_test(Maybe<T>& m) {
+	if (m) exit(1);
+	T value(123);
+	m = std::move(value);
+	if (m.get()->n != 123) exit(1);
+	m = std::move(value);
+	if (m.get()->n != 123) exit(1);
+}
+
+int main (int argc, char const *argv[])
+{
+	{
+		Maybe<TrivialType> m;
+		m = TrivialType(123);
+		TrivialType t(123);
+		m = t;
+	}
+	
+	{
+		Maybe<OnlyCopyConstructible> m(OnlyCopyConstructible(123));
+		m = OnlyCopyConstructible(123);
+		OnlyCopyConstructible v(123);
+		m = v;
+	}
+	
+	{
+		Maybe<OnlyMoveConstructible> m(OnlyMoveConstructible(123));
+		m = OnlyMoveConstructible(123);
+		OnlyMoveConstructible v(123);
+		m = std::move(v);
+	}
+	
+	{
+		Maybe<OnlyCopyAssignable> m(OnlyCopyAssignable(123));
+		m = OnlyCopyAssignable(123);
+		OnlyCopyAssignable v(123);
+		m = v;
+	}
+	
+	{
+		Maybe<OnlyMoveAssignable> m(OnlyMoveAssignable(123));
+		m = OnlyMoveAssignable(123);
+		OnlyMoveAssignable v(123);
+		m = std::move(v);
+	}
+	
+	{
+		Maybe<CopyConstructibleAndAssignable> m(CopyConstructibleAndAssignable(123));
+		m = CopyConstructibleAndAssignable(123);
+		CopyConstructibleAndAssignable v(123);
+		m = v;
+	}
+	
+	{
+		Maybe<MoveConstructibleAndAssignable> m(MoveConstructibleAndAssignable(123));
+		m = MoveConstructibleAndAssignable(123);
+		MoveConstructibleAndAssignable v(123);
+		m = std::move(v);
+	}
+	
+	{
+		Maybe<CopyConstructibleAndMoveAssignable> m(CopyConstructibleAndMoveAssignable(123));
+		m = CopyConstructibleAndMoveAssignable(123);
+		CopyConstructibleAndMoveAssignable v(123);
+		m = std::move(v);
+	}
+	
+	{
+		Maybe<MoveConstructibleAndCopyAssignable> m(MoveConstructibleAndCopyAssignable(123));
+		m = MoveConstructibleAndCopyAssignable(123);
+		MoveConstructibleAndCopyAssignable v(123);
+		m = v;
+	}
+	
+	{
+		Maybe<CopyAndMoveConstructibleCopyAssignable> m(CopyAndMoveConstructibleCopyAssignable(123));
+		CopyAndMoveConstructibleCopyAssignable v(123);
+		Maybe<CopyAndMoveConstructibleCopyAssignable> m2(v);
+		Maybe<CopyAndMoveConstructibleCopyAssignable> m3(std::move(v));
+		m = v;
+	}
+	
+	{
+		Maybe<CopyAndMoveConstructibleMoveAssignable> m(CopyAndMoveConstructibleMoveAssignable(123));
+		CopyAndMoveConstructibleMoveAssignable v(123);
+		m = std::move(v);
+		Maybe<CopyAndMoveConstructibleMoveAssignable> m2 = v;
+	}
+	
+	{
+		CopyConstructibleCopyAndMoveAssignable v(123);
+		Maybe<CopyConstructibleCopyAndMoveAssignable> m(v);
+		m = std::move(v);
+	}
+	
+	{
+		MoveConstructibleCopyAndMoveAssignable v(123);
+		Maybe<MoveConstructibleCopyAndMoveAssignable> m(std::move(v));
+		m = std::move(v);
+		m = v;
+	}
+	
+	{
+		Maybe<NoDefaultConstructor> m = NoDefaultConstructor(123);
+		m = NoDefaultConstructor(123);
+		NoDefaultConstructor v(456);
+		m = v;
+	}
+	
+	return 0;
+}
